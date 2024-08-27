b865cd340cb4b3ed9f8ff1b70c977e84971e59e9
AdrianaMillares <69483582+AdrianaMillares@users.noreply.github.com>
2024-08-27 11:59:04 -0600

    fix navbar

--- mural-frontend/src/components/Navbar.js
+++ mural-frontend/src/components/Navbar.js
@@ -1,61 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { FireQuestion } from '../utils/alertHandler';
-import { LogOut, Key } from 'react-feather';
-import { logOut } from '../utils/auth';
+import { LogOut } from 'react-feather';
+import { isAuthenticated, logOut } from '../utils/auth';
 import routes from '../routes';
 import '../styles/navbar.css';
-import { isAuthenticated } from '../utils/auth';
 
 function Navbar() {
     const navigate = useNavigate();
-    const location = useLocation(); // para obtener la ruta actual
-    const [activeRoute, setActiveRoute] = useState(location.pathname);
-
-    useEffect(() => {
-        setActiveRoute(location.pathname);
-    }, [location.pathname]); // Cambia la ruta activa cuando cambia la ubicación
+    const location = useLocation(); // Obtener la ruta actual
+    const [isOpen, setIsOpen] = useState(false); // Estado para controlar el menú
 
     const logOutHandler = async () => {
-        const confirmation = await FireQuestion(
-            '¿Está seguro de que quiere cerrar sesión?',
-            'Tendrá que iniciar sesión de nuevo para volver a entrar.',
-            'Cerrar sesión'
-        );
-        if (confirmation.isDismissed) return;
         logOut();
         navigate('/login');
     };
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div className='navbar'>
-            <div>
+        <div className="navbar">
+            <div className="navbar-brand">
+                <button className="hamburger" onClick={toggleMenu}>
+                    &#9776; {/* Icono de hamburguesa */}
+                </button>
+            </div>
+            <div className={`navbar-menu ${isOpen ? 'is-active' : ''}`}>
                 {routes
-                    .filter((route) => route.inNavbar)
-                    .map((route) => (
+                    .filter(route => route.inNavbar)
+                    .map(route => (
                         <a
                             key={route.path}
-                            className={`navbar-button ${activeRoute === route.path ? 'active' : ''}`}
+                            className={`navbar-button ${location.pathname === route.path ? 'active' : ''}`}
                             onClick={() => {
-                                setActiveRoute(route.path);
                                 navigate(route.path);
+                                setIsOpen(false); // Cierra el menú después de la selección
                             }}
                         >
                             {route.name}
                         </a>
                     ))}
             </div>
-            <div className='navbar-logout'>
+            <div className="navbar-logout">
                 {isAuthenticated() && (
                     <a onClick={logOutHandler}>
                         <span>Cerrar sesión</span>
-                        <LogOut color='white' />
+                        <LogOut color="white" />
                     </a>
                 )}
-                {/* <a onClick={() => navigate('/login')}>
-                    <span>Iniciar sesión</span>
-                    <Key color='white' />
-                </a> */}
             </div>
         </div>
     );
